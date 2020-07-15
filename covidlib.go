8c54460f3d9566b9c0d77ae3e4e5845eab12d32b
uzma012 <67821856+uzma012@users.noreply.github.com>
2020-07-15 22:11:22 +0500

    Add files via upload

---
+++ covidlib.go
@@ -0,0 +1,71 @@
+package covidlib
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strings"
+)
+
+type covid struct {
+	Date    string
+	Discharged    string
+	Expired  string
+	Region string
+	Still_admitted string
+	Test string
+	Positive string
+}
+
+func Load(path string) []covid {
+	table := make([]covid, 0)
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+		c := covid{
+			Date: row[4],
+			Test: row[3],
+			Positive:row[2],
+			Discharged:    row[5],
+			Expired:    row[6],
+			Region:  row[7],
+			Still_admitted: row[8],
+		}
+		table = append(table, c)
+	}
+	return table
+}
+
+func Find(table []covid, filter string) []covid {
+	if filter == "" || filter == "*" {
+		return table
+	}
+	result := make([]covid, 0)
+	filter = strings.ToUpper(filter)
+	for _, cur := range table {
+		if cur.Discharged== filter ||
+			cur.Still_admitted == filter ||
+			cur.Test == filter ||
+			cur.Positive == filter ||
+			cur.Expired == filter ||
+			strings.Contains(strings.ToUpper(cur.Region), filter) ||
+			strings.Contains(strings.ToUpper(cur.Date), filter) {
+			result = append(result, cur)
+
+		}
+	}
+	
+	return result
+}
---
+++ tcp.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	curr "github.com/uzma012/Coronavirus-report"
+)
+
+var detail = curr.Load(".../covid_final_data.csv")
+
+func main() {
+	var addr string
+	var network string
+	flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.Parse()
+
+	// validate supported network protocols
+	switch network {
+	case "tcp", "tcp4", "tcp6", "unix":
+	default:
+		log.Fatalln("unsupported network protocol:", network)
+	}
+
+	// create a listener for provided network and host address
+	ln, err := net.Listen(network, addr)
+	if err != nil {
+		log.Fatal("failed to create listener:", err)
+	}
+	defer ln.Close()
+	log.Println("**** Pakistan covid Coronavirus-report ***")
+	log.Printf("Service started: (%s) %s\n", network, addr)
+
+	// connection-loop - handle incoming requests
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			if err := conn.Close(); err != nil {
+				log.Println("failed to close listener:", err)
+			}
+			continue
+		}
+		log.Println("Connected to", conn.RemoteAddr())
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println("error closing connection:", err)
+		}
+	}()
+
+	if _, err := conn.Write([]byte("Connected...\nUsage: GET <region, or date>\n")); err != nil {
+		log.Println("error writing:", err)
+		return
+	}
+
+	// loop to stay connected with client until client breaks connection
+	for {
+		// buffer for client command
+		cmdLine := make([]byte, (1024 * 4))
+		n, err := conn.Read(cmdLine)
+		if n == 0 || err != nil {
+			log.Println("connection read error:", err)
+			return
+		}
+		cmd, param := parseCommand(string(cmdLine[0:n]))
+		if cmd == "" {
+			if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+				log.Println("failed to write:", err)
+				return
+			}
+			continue
+		}
+
+		// execute command
+		switch strings.ToUpper(cmd) {
+		case "GET":
+			result := curr.Find(detail, param)
+			if len(result) == 0 {
+				if _, err := conn.Write([]byte("Nothing found\n")); err != nil {
+					log.Println("failed to write:", err)
+				}
+				continue
+			}
+			// send each currency info as a line to the client wiht fmt.Fprintf()
+			for _, cur := range result {
+				_, err := conn.Write([]byte(
+					fmt.Sprintf(
+						"%s %s %s %s %s \n",
+						cur.Date,cur.Test,cur.Positive,cur.Discharged, cur.Expired, cur.Region,cur.Still_admitted,
+					),
+				))
+				if err != nil {
+					log.Println("failed to write response:", err)
+					return
+				}
+			}
+
+		default:
+			if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+				log.Println("failed to write:", err)
+				return
+			}
+		}
+	}
+}
+
+func parseCommand(cmdLine string) (cmd, param string) {
+	parts := strings.Split(cmdLine, " ")
+	if len(parts) != 2 {
+		return "", ""
+	}
+	cmd = strings.TrimSpace(parts[0])
+	param = strings.TrimSpace(parts[1])
+	return
+}
